decb003e1b9b2b12ab6e86b55c2c3a8cc5273b6c
jAtInn71 <161357209+jAtInn71@users.noreply.github.com>
2025-09-04 23:10:39 +0530

    Fix import case for Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
 import Projects from "./pages/Projects";
