acb59504ffd426640f0bbcdb9a8961191cfd3a34
jAtInn71 <161357209+jAtInn71@users.noreply.github.com>
2025-10-22 21:30:15 +0530

    Reorder menu items in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,8 +93,8 @@ const Navbar = () => {
     { href: "home", label: "Home" },
     { href: "about", label: "About" },
     { href: "skills", label: "Skills" },
-    { href: "certificates", label: "Certificates" },
     { href: "projects", label: "Projects" },
+    { href: "certificates", label: "Certificates" },
     { href: "contact", label: "Contact" },
   ];
 
